8f09cdfa34838e82c023b691ef793ec8bf2f5daf
Natividad Perez <nati@debian>
2025-05-26 15:32:02 -0600

    hola api que no entiendo de musicbrainz

---
+++ investigacion/sobre-la-construccion/programas/hola-musicbrainz.py
@@ -0,0 +1,28 @@
+"""
+Lo que intento hacer cone sto es relaizar busquedas simples y terminar de dimencionar cómo funcionan las consultas
+A futuro busco generar un buscador de canciones pertenecientes a ciertos generos en un intervalo de años especifico
+"""
+
+
+import requests # libreria para acceder a la API 
+
+# endpoint de la base de musicbrainz que vamos a visitar
+base_url = "https://musicbrainz.org/ws/2"
+# cosas que pide musicbrainz para identificarme
+headers = {'User-Agent': 'buscador-natits/1.0 (a357417@uach.mx)'}
+
+# parametros de la consulta
+params = {
+    'query': 'tag:salsa', 
+    'fmt': 'json',
+    'limit': 20
+}
+
+# url a visitar
+url = f'{base_url}/release-group'
+
+respuesta = requests.get(url, headers=headers, params=params).json()['release-groups']
+
+for i in respuesta:
+    print(f'{i}\n')
+
