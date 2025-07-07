b920d7ec81fc672ec6add01adc60f2c22a43d56f
Natividad Perez <nati@debian>
2025-07-07 13:05:49 -0600

    vamos avanzando en la construccion de las series que contendran los datos de la cancion, ya sabemos cuantos espacios necesitamos y ahora solo falta iterar sobre los datos e ir llenando nuestro arreglo con la estructura adecuada y calculando promedios en caso de que haya eventos multiples

--- investigacion/sobre-la-construccion/programas/midi-csv/midi-csv.py
+++ investigacion/sobre-la-construccion/programas/midi-csv/midi-csv.py
@@ -9,20 +9,73 @@ queremos visitar todos los archivos midi dentro de una carpeta y con cada uno ha
 import pretty_midi
 import pandas
 
+# leemos el midi y lo guardamos con la estructura de pretty_midi
 midi = pretty_midi.PrettyMIDI('frankieruiz-tu-con-el.mid')
-datos = []
+
+# para almacenar los diferentes datos que componen la canción
+eventos = []
+
+# iteramos en cada instrumento del midi
 for instrument in midi.instruments:
-    for note in instrument.notes:
-        datos.append({
-            'nombre': f'{instrument.program}',
-            'nota': f'{note.pitch}',
-            'empieza': f'{note.start}',
-            'termina': f'{note.end}',
-            'volumen': f'{note.velocity}'
-        })
+    # para saber si el isntrumento corresponde a percuciones
+    if instrument.is_drum:
+        print("\nmira nomais")
+        name = -1
+    else:
+        name = instrument.program
         
+    # iteramos en cada nota compone la canción
+    for note in instrument.notes:
+        eventos.append({
+            'instrumento': name,
+            'nota': note.pitch,
+            'volumen': note.velocity,
+            'dura': note.end - note.start,
+            'empieza': note.start,
+            'termina': note.end
+        })
 
+# convertimos la lista en un dat frame para poder operar con ella
+cancion = pandas.DataFrame(eventos)   
+cancion.to_csv('cancion.csv', index=False) # lo guardamos en la carpeta
+# agrupamos para tener por separado cada instrumento de los eventos
+instrumentos = cancion.groupby('instrumento')
 
-print(f'{datos}')
+print(f'\ntenemos un total de {len(instrumentos)} instrumentos que componen la canción')
+
+# para almacenar los máximos y minimos de cada instrumento
+max_t = [] # tiempos máximos
+min_t = [] # tiempos mínimose
+min_d = [] # duraciones mínimas
+
+# iteramos sobre los intrumentos para almacenar los datos en los arreglos
+for i, ins in instrumentos:
+    print(f'\npara el instrumento {i} son {len(ins)} notas')
+    print(f'con tiempos que van desde {ins["empieza"].min()} hasta {ins["termina"].max()}')
+    print(f'el intervalo más pequeño es de {ins["dura"].min()}')
     
+    # almacenamos los mínimos y máximos en cada arreglo
+    max_t.append(float(ins["termina"].max()))
+    min_t.append(float(ins["empieza"].min()))
+    min_d.append(float(ins["dura"].min()))
 
+duracion = max(max_t) - min(min_t)
+espacios = int(duracion/min(min_d))
+print(f'\nentonces sabemos que todos las notas de todos los instrumentos ocurren en un intervalo de {duracion} segundos y que el intérvalo más pequeño es {min(min_d)}')
+print(f'\nla canción dura entonces {duracion/60} minutos y vamos a ocupar listas de {espacios} espacios para almacenar todos los eventos')
+
+# construimos la lista con ceros que contendrá los datos
+rolita = []
+for i in range(espacios):
+    estructura = {
+        'instrumento': 0,
+        'nota': 0,
+        'volumen': 0,
+        'dura': 0
+    }
+    rolita.append(estructura)
+    
+    
+for i, evento in rolita:
+    
+    
